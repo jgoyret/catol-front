5c2e98c35aa342079329a56eacf70f0f10a4f306
Juan Goyret <j.goyret1475@gmail.com>
2024-11-11 15:42:23 -0300

    comming soon 1

--- src/App.tsx
+++ src/App.tsx
@@ -156,7 +156,7 @@ function App() {
   const [isReady, setIsReady] = React.useState(false);
 
   React.useEffect(() => {
-    setIsReady(true);
+    setIsReady(false);
   }, []);
 
   return <>{isReady ? <RouterProvider router={router} /> : <ComingSoon />}</>;
